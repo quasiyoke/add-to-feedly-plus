64867cd783ad0aede7105ca522124ad653186963
Pyotr Ermishkin <quasiyoke@gmail.com>
2015-02-16 15:01:45 +0300

    Duplicate feeds handling was added.

--- data/content.js
+++ data/content.js
@@ -1,12 +1,30 @@
+function Page () {
+	this.feeds = [];
+}
+
+Page.prototype.pushFeed = function (feed) {
+	/**
+	 * Pays attention for removing duplicate feeds.
+	 */
+	for (var i = 0; i < this.feeds.length; ++i) {
+		var thisFeed = this.feeds[i];
+		if (thisFeed.url === feed.url) {
+			if (feed.title && (!thisFeed.title || thisFeed.title.length < feed.title.length)) { // If new feed's title is better than existing, replace old one.
+				thisFeed.title = feed.title;
+			}
+			return;
+		}
+	}
+	this.feeds.push(feed);
+};
+
 self.port.on("processPage", function () {
-	var page = {
-		feeds: []
-	};
+	var page = new Page();
 	
 	var links = document.getElementsByTagName("link"); 
 	for (var i = 0; i < links.length; ++i)	{
-		if ("application/rss+xml" == links[i].getAttribute("type"))	{
-			page.feeds.push({
+		if ("application/rss+xml" === links[i].getAttribute("type"))	{
+			page.pushFeed({
 				title: links[i].getAttribute("title"),
 				url: links[i].href
 			});
