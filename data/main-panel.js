e82aed781df21e8caba27d17359350d01f6c8c7c
Pyotr Ermishkin <quasiyoke@gmail.com>
2015-02-20 07:35:22 +0300

    Trailing » stripping was added at common title. Smart adsorbing common titles handling was implemented.

--- data/main-panel.js
+++ data/main-panel.js
@@ -22,7 +22,22 @@ function addTitle (text) {
 	menu.parentNode.insertBefore(title, menu);
 }
 
-function getCommonTitle (feeds) {
+function Feed (options) {
+	this.url = options.url;
+	this.title = options.title || options.url;
+}
+
+Feed.getCommonTitle = function (feeds) {
+	/**
+	 * The idea behind common title is to get common header for feeds with titles starting identically.
+	 * Imagine the following feeds list:
+	 *
+	 * - The Time » New Articles
+	 * - The Time » Editor's blog
+	 * - The Time » Comments
+	 *
+	 * In such case our desire is to get common title "The Time » ".
+	 */
 	if (feeds.length <= 1) {
 		return "";
 	}
@@ -31,13 +46,66 @@ function getCommonTitle (feeds) {
 	for (length = 0; length < firstTitle.length; ++length) {
 		var c = firstTitle.charAt(length);
 		for (var i = 1; i < feeds.length; ++i) {
-			if (feeds[i].title.charAt(length) != c) {
+			if (feeds[i].title.charAt(length) !== c) {
 				break outer;
 			}
 		}
 	}
-	return firstTitle.substr(0, length);
-}
+	var commonTitle = firstTitle.substr(0, length);
+	if (Feed.isCommonTitleAdsorbing(commonTitle, feeds)) {
+		var match = /^(.+»)[^»]+$/.exec(commonTitle);
+		/*
+		 * For such feeds list:
+		 *
+		 * - John Doe's website » Blog
+		 * - John Doe's website » Blog comments
+		 *
+		 * ...common title will be: "John Doe's website »"
+		 */
+		if (match) {
+			commonTitle = match[1];
+			/*
+			 * For such feeds list:
+			 *
+			 * - John Doe's blog
+			 * - John Doe's blog comments
+			 *
+			 * ...common title will be: "John Doe's"
+			 */
+		} else if (match = /^(.*)\s[^\s]+\s*$/.exec(commonTitle)) {
+			commonTitle = match[1];
+		} else {
+			commonTitle = '';
+		}
+	}
+	return commonTitle;
+};
+
+Feed.isCommonTitleAdsorbing = function (commonTitle, feeds) {
+	/**
+	 * Example feeds list:
+	 *
+	 * - John Doe's blog
+	 * - John Doe's blog comments
+	 *
+	 * In such case straightforward common title will adsorb first feed's title.
+	 *
+	 * @return bool Is common title adsorbing any feed's title from feeds list.
+	 */
+	for (var i = 0; i < feeds.length; ++i) {
+		if (commonTitle.length >= feeds[i].title.length) {
+			return true;
+		}
+	}
+	return false;
+};
+
+Feed.prototype.ensureTitle = function (commonTitleLength) {
+	/**
+	 * Ensures the title is trimmed correctly.
+	 */
+	this.title = this.title.substr(commonTitleLength);
+};
 
 self.port.on("show", function (page) {
 	/* Remove title if present. */
@@ -47,22 +115,22 @@ self.port.on("show", function (page) {
 	}
 	
 	menu.innerHTML = "";
-	var commonTitle = getCommonTitle(page.feeds);
+	var commonTitle = Feed.getCommonTitle(page.feeds);
 	var commonTitleLength;
 	if (commonTitle) {
-		addTitle(commonTitle);
 		commonTitleLength = commonTitle.length;
+		/* Trim trailing » char. */
+		var match = /^(.+)»\s*/.exec(commonTitle);
+		if (match) {
+			commonTitle = match[1];
+		}
+		addTitle(commonTitle);
 	} else {
 		commonTitleLength = 0;
 	}
 	for (var i = 0; i < page.feeds.length; ++i) {
-		var feed = page.feeds[i];
-		if (feed.title) {
-			feed.title = feed.title.substr(commonTitleLength);
-		}
-		if (!feed.title) {
-			feed.title = feed.url;
-		}
+		var feed = new Feed(page.feeds[i]);
+		feed.ensureTitle(commonTitleLength);
 		addFeed(feed);
 	}
 });
