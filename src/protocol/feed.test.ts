7711a8b253b1c37251d3abe73aa015b1c2bc63e8
Petr Ermishkin <petr@quasiyoke.me>
2025-08-24 01:07:38 +0300

    Fix subscription link: aggregator now URL-encodes feed URL in it
    Aggregator has changed the rules for generating links containing feed
    URLs:
    
    1. Now aggregator requires feed URLs to be URL-encoded and redirects
       if they are not.
    2. In an attempt to redirect to a "fixed" link, aggregator loses
       the second slash after the feed's URL schema :// — breaking it.

--- src/protocol/feed.test.ts
+++ src/protocol/feed.test.ts
@@ -5,10 +5,26 @@ import { subscriptionUrl, type Feed } from './feed.ts';
 describe('subscriptionUrl', () => {
   describe('for feed', () => {
     it('builds aggregator subscription URL', () => {
-      expect(
-        subscriptionUrl(feedStubWithUrl('https://blog.mozilla.org/en/feed/')),
-      ).toBe(
-        'https://feedly.com/i/subscription/feed/https://blog.mozilla.org/en/feed/',
+      const feed = feedStubWithUrl('https://blog.mozilla.org/en/feed/');
+      expect(subscriptionUrl(feed)).toBe(
+        'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fblog.mozilla.org%2Fen%2Ffeed%2F',
+      );
+    });
+  });
+
+  describe('for feed with URL having querystring', () => {
+    it('encodes querystring correctly', () => {
+      const feed = feedStubWithUrl(
+        'https://wiki.mozilla.org/index.php?title=Special:RecentChanges&feed=atom',
+      );
+      expect(subscriptionUrl(feed)).toBe(
+        'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwiki.mozilla.org%2Findex.php%3Ftitle%3DSpecial%3ARecentChanges%26feed%3Datom',
+      );
+      const feed2 = feedStubWithUrl(
+        'https://www.youtube.com/feeds/videos.xml?channel_id=UC_iD0xppBwwsrM9DegC5cQQ',
+      );
+      expect(subscriptionUrl(feed2)).toBe(
+        'https://feedly.com/i/subscription/feed%2Fhttps%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3DUC_iD0xppBwwsrM9DegC5cQQ',
       );
     });
   });
