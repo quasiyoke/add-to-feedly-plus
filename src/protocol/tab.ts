89fd45aa926e86861c418a3da4428d2aa3ed2db2
Petr Ermishkin <petr@quasiyoke.me>
2025-08-30 17:01:58 +0300

    Introduce "TabId" type

--- src/protocol/tab.ts
+++ src/protocol/tab.ts
@@ -1,5 +1,24 @@
+import type { Tabs } from 'webextension-polyfill';
+
+import type { Brand } from '@/util.ts';
 import type { Page } from '@/protocol/page.ts';
 
 export type Tab = {
   pageInfo?: Page;
 };
+
+export type TabId = Brand<'TabId', number>;
+
+export function activatedTabId(
+  activation: Tabs.OnActivatedActiveInfoType,
+): TabId {
+  return tabId(activation.tabId);
+}
+
+export function browserTabId(tab: Tabs.Tab): TabId | undefined {
+  return tab.id == null ? undefined : tabId(tab.id);
+}
+
+function tabId(id: number): TabId {
+  return id as unknown as TabId;
+}
