082b3ec000bf29e211a3b5a2280d275c00dce00b
Petr Ermishkin <petr@quasiyoke.me>
2025-09-06 15:11:42 +0300

    Chrome: action badge

--- src/pageAction/index.test.ts
+++ src/pageAction/index.test.ts
@@ -3,25 +3,77 @@ import { expect, it, describe } from 'vitest';
 import type { Feed } from '@/protocol/feed.ts';
 import { onlyForTesting as pageAction } from './index.ts';
 
-const { label } = pageAction;
+const { badge, actionLabel, pageActionLabel } = pageAction;
 
-describe('label', () => {
+describe('badge', () => {
+  describe('for empty feeds list', () => {
+    it('returns null', () => {
+      expect(badge([])).toBeNull();
+    });
+  });
+
+  describe('for a single-digit feeds count', () => {
+    it('produces count', () => {
+      expect(badge(feeds(1))).toBe('1');
+      expect(badge(feeds(9))).toBe('9');
+    });
+  });
+
+  describe('for a long feeds list', () => {
+    it('nominally indicates "many feeds"', () => {
+      expect(badge(feeds(10))).toBe('∞');
+      expect(badge(feeds(11))).toBe('∞');
+    });
+  });
+});
+
+describe('pageActionLabel', () => {
   describe('for empty feeds list', () => {
     it('returns placeholder text', () => {
-      expect(label([], '')).toBe('Add to Feedly Plus (no feeds)');
+      expect(pageActionLabel([], '')).toBe('Add to Feedly Plus (no feeds)');
     });
   });
 
   describe('for a single feed', () => {
     it('builds label containing title', () => {
-      expect(label(feeds(1), '')).toBe('Add to Feedly: “Example feed 0”');
+      expect(pageActionLabel(feeds(1), '')).toBe(
+        'Add to Feedly: “Example feed 0”',
+      );
     });
   });
 
   describe('for a long feeds list', () => {
     it('builds label containing count', () => {
-      expect(label(feeds(2), '')).toBe('Add to Feedly (2)');
-      expect(label(feeds(10), '')).toBe('Add to Feedly (10)');
+      expect(pageActionLabel(feeds(2), '')).toBe('Add to Feedly (2)');
+      expect(pageActionLabel(feeds(10), '')).toBe('Add to Feedly (10)');
+    });
+  });
+});
+
+describe('actionLabel', () => {
+  describe('for empty feeds list', () => {
+    it('returns placeholder text', () => {
+      expect(actionLabel([], '')).toBe('Add to Feedly Plus (no feeds)');
+    });
+  });
+
+  describe('for a single feed', () => {
+    it('builds label containing title', () => {
+      expect(actionLabel(feeds(1), '')).toBe('Add to Feedly: “Example feed 0”');
+    });
+  });
+
+  describe('for a feeds list of length 2...9 inclusively', () => {
+    it('returns extension name', () => {
+      expect(actionLabel(feeds(2), '')).toBe('Add to Feedly Plus');
+      expect(actionLabel(feeds(9), '')).toBe('Add to Feedly Plus');
+    });
+  });
+
+  describe('for a feeds list of length starting from 10', () => {
+    it('builds label containing count', () => {
+      expect(actionLabel(feeds(10), '')).toBe('Add to Feedly (10)');
+      expect(actionLabel(feeds(11), '')).toBe('Add to Feedly (11)');
     });
   });
 });
